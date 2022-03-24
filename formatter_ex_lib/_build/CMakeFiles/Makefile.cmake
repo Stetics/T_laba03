9ef66d3c42c6c7e08856a0e9b3ebae5093f6fc1c
Stetics Seinen <maxira626@gmail.com>
2022-03-25 01:25:05 +0300

    new test

--- formatter_ex_lib/_build/CMakeFiles/Makefile.cmake
+++ formatter_ex_lib/_build/CMakeFiles/Makefile.cmake
@@ -11,6 +11,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.16.3/CMakeCCompiler.cmake"
   "CMakeFiles/3.16.3/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.16.3/CMakeSystem.cmake"
+  "/home/inspired/T_lb3/formatter_lib/CMakeLists.txt"
   "/usr/share/cmake-3.16/Modules/CMakeCCompiler.cmake.in"
   "/usr/share/cmake-3.16/Modules/CMakeCCompilerABI.c"
   "/usr/share/cmake-3.16/Modules/CMakeCInformation.cmake"
@@ -112,9 +113,11 @@ set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/3.16.3/CMakeCCompiler.cmake"
   "CMakeFiles/3.16.3/CMakeCXXCompiler.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
+  "formatter_lib/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
   )
--- formatter_ex_lib/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake
+++ formatter_ex_lib/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake
@@ -4,19 +4,17 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/inspired/T_lb3/formatter_ex_lib/formatter_ex.cpp" "/home/inspired/T_lb3/formatter_ex_lib/_build/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
+  "/home/inspired/T_lb3/formatter_lib/formatter.cpp" "/home/inspired/T_lb3/formatter_ex_lib/_build/formatter_lib/CMakeFiles/formatter.dir/formatter.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../"
   "/home/inspired/T_lb3/formatter_lib"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/inspired/T_lb3/formatter_ex_lib/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
